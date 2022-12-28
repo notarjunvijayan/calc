fd76bf3ada721d00dfcc37dd1806faa013a72044
Arjun Vijayan <vijayanarjun4@gmail.com>
2022-12-28 20:22:22 +0530

    Working calculator Update

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,16 +1,27 @@
 import { useState } from "react"
 
 function Calculator(){
-    const [input, setInput] = useState('')
+    const [input, setInput] = useState("")
     
+    // Update Input Field
     function update(val){
-     setInput(input + val)
+     setInput(input + String(val))
     }
+    // Remove last Item from input Field
     function remove(){
         setInput(input.substring(0,input.length-1) )
     }
+    // Reset Input Field
     function reset(){
         setInput('')
+    }
+
+    //Final Caluculation
+    function calculate(){
+        var data = input.replace(/[^-()\d/*+.]/g, '');
+        let result = 0
+        result = eval(data)
+        setInput(result)
     }
     return(
         <>
@@ -30,6 +41,16 @@ function Calculator(){
         <button onClick = {remove}>C</button>
         <button onClick = {() => (update(0))}>0</button>
         <button onClick = {reset}>CE</button>
+        </div>
+        <div className="spec-but">
+            <button onClick = {() =>(update('+'))}>+</button>
+            <button onClick = {() =>(update('-'))}>-</button>
+            <button onClick = {() =>(update('*'))}>*</button>
+            <button onClick = {() =>(update('/'))}>/</button>
+            <button onClick = {() => (update('('))}>(</button>
+            <button onClick = {() => (update(')'))}>)</button>
+            <button onClick = {() => (update('.'))}>.</button>
+            <button onClick = {calculate}>=</button>
         </div>
         </>
         
