f0df6b38e58db5dceca81317f47e657995e5e4d8
Arjun Vijayan <vijayanarjun4@gmail.com>
2022-12-29 16:00:13 +0530

    animation and button ui

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
 import Calculator from './Calculator';
+import {motion, spring} from 'framer-motion'
 
 function App() {
   return (
     <div className="App">
       <div className="heading">
         <div>
-          <h1>Calculator</h1>
-          <p>By <a href='https://www.notarjunvijayan.me' target='_blank'>Arjun Vijayan</a></p>
+          <motion.div
+          animate= {{y:5}}
+          transition = {{type: 'spring',duration: 1}}
+          >
+            <h1>Calculator</h1>
+          </motion.div>
+          <motion.div
+          animate = {{y:5}}
+          transition = {{type:'spring',duration:1}}>
+            <p>By <a href='https://www.notarjunvijayan.me' target='_blank' rel="noreferrer">Arjun Vijayan</a></p>
+          </motion.div>
         </div>
       </div>
+      <motion.div
+      animate = {{y:5}}
+      transition = {{type: 'spring', duration: 1,}}>
       <div className="container">
       <div className="canvas">
         <Calculator/>
       </div>
       </div>
+      </motion.div>
+      
     </div>
   );
 }
